34bf94feb03658263bdb370b4f2fa2f1918ecb59
dungducphan <brianp.dung@utexas.edu>
2022-06-08 13:48:36 -0500

    Adding messenger object to handle user-defined variables at runtime.

--- inc/detcon.hh
+++ inc/detcon.hh
@@ -21,6 +21,7 @@
 #include "G4LogicalBorderSurface.hh"
 #include "G4LogicalSkinSurface.hh"
 #include "G4SDManager.hh"
+#include "G4GenericMessenger.hh"
 
 #include "TMath.h"
 
@@ -32,25 +33,55 @@ class detcon : public G4VUserDetectorConstruction {
 public:
     detcon();
     virtual ~detcon();
-
     virtual G4VPhysicalVolume *Construct();
+    virtual void DefineMaterials();
+    virtual void DefineMessenger();
 
 private:
+    G4GenericMessenger *mMessenger = nullptr;
+
+private:
+    G4NistManager *nist = nullptr;
+    G4Material *worldMat = nullptr;
+    G4Material *al = nullptr;
+    G4Material *ptp = nullptr;
+    G4Material *dcf = nullptr;
+    G4Material *tpb = nullptr;
+    G4Material *kali = nullptr;
+
+private:
+    G4double worldSize = 15 * cm;
+    G4double detIR = 4*cm;
+    G4double wallThickness = 1*cm;
+    G4double detOR = detIR + wallThickness;
+    G4double detHalfHeight = 0.4*cm;
+    G4double ptpHalfHeight = 50*um;
+    G4double dcfHalfHeight = 0.1*cm;
+    G4double tpbHalfHeight = 50*um;
+    G4double pmtHalfHeight = 0.1*cm;
+
+private:
+    G4Box *solidWorld = nullptr;
     G4LogicalVolume* logicWorld = nullptr;
     G4VPhysicalVolume* physWorld = nullptr;
 
+    G4Tubs *solidAl = nullptr;
     G4LogicalVolume* logicAl = nullptr;
     G4VPhysicalVolume* physAl = nullptr;
 
+    G4Tubs *solidPTP = nullptr;
     G4LogicalVolume* logicPTP = nullptr;
     G4VPhysicalVolume* physPTP = nullptr;
 
+    G4Tubs *solidDCF = nullptr;
     G4LogicalVolume* logicDCF = nullptr;
     G4VPhysicalVolume* physDCF = nullptr;
 
+    G4Tubs *solidTPB = nullptr;
     G4LogicalVolume* logicTPB = nullptr;
     G4VPhysicalVolume* physTPB = nullptr;
 
+    G4Tubs *solidPMT = nullptr;
     G4LogicalVolume* logicPMT = nullptr;
     G4VPhysicalVolume* physPMT = nullptr;
 };
--- src/detcon.cc
+++ src/detcon.cc
@@ -1,61 +1,78 @@
 #include "detcon.hh"
 
-detcon::detcon() : G4VUserDetectorConstruction() {}
+detcon::detcon() : G4VUserDetectorConstruction(),
+                   worldSize(15*cm),
+                   detIR(4*cm),
+                   wallThickness(1*cm),
+                   detHalfHeight(0.4*cm),
+                   ptpHalfHeight(50*um),
+                   dcfHalfHeight(0.1*cm),
+                   tpbHalfHeight(50*um),
+                   pmtHalfHeight(0.1*cm) {
+    DefineMessenger();
+    DefineMaterials();
+}
 
 detcon::~detcon() {}
 
+void detcon::DefineMessenger() {
+    mMessenger = new G4GenericMessenger(this, "/detCon/", "Detector Geometry Messenger");
+    mMessenger->DeclareProperty("detIR",         detIR,         "Detector case inner radius");
+    mMessenger->DeclareProperty("wallThickness", wallThickness, "Detector case thickness");
+    mMessenger->DeclareProperty("detHalfHeight", detHalfHeight, "Detector half height");
+    mMessenger->DeclareProperty("ptpHalfHeight", ptpHalfHeight, "PTP layer half height");
+    mMessenger->DeclareProperty("dcfHalfHeight", dcfHalfHeight, "DCF layer half height");
+    mMessenger->DeclareProperty("tpbHalfHeight", tpbHalfHeight, "TPB layer half height");
+    mMessenger->DeclareProperty("pmtHalfHeight", pmtHalfHeight, "PMT layer half height");
+}
+
+void detcon::DefineMaterials() {
+    nist = G4NistManager::Instance();
+    worldMat = nist->FindOrBuildMaterial("G4_Galactic");
+    al = nist->FindOrBuildMaterial("G4_Al");
+    ptp = nist->FindOrBuildMaterial("G4_TERPHENYL");
+    dcf = nist->FindOrBuildMaterial("G4_SILICON_DIOXIDE");
+    tpb = nist->FindOrBuildMaterial("G4_ANTHRACENE");
+    kali = nist->FindOrBuildMaterial("G4_K");
+}
+
 G4VPhysicalVolume *detcon::Construct() {
-    G4NistManager *nist = G4NistManager::Instance();
     G4bool checkOverlaps = true;
 
     // World
-    G4double worldSize = 15 * cm;
-    G4Material *worldMat = nist->FindOrBuildMaterial("G4_Galactic");
-    G4Box *solidWorld = new G4Box("solidWorld", 0.5 * worldSize, 0.5 * worldSize, 0.5 * worldSize);
+    solidWorld = new G4Box("solidWorld", 0.5 * worldSize, 0.5 * worldSize, 0.5 * worldSize);
     logicWorld = new G4LogicalVolume(solidWorld, worldMat, "logicWorld");
     physWorld = new G4PVPlacement(0, G4ThreeVector(), logicWorld, "physWorld", 0, false, 0, checkOverlaps);
 
     // Aluminum Case
-    G4double detIR = 4*cm;
-    G4double wallThickness = 1*cm;
-    G4double detOR = detIR + wallThickness;
-    G4double detHalfHeight = 0.4*cm;
-    G4Material *al = nist->FindOrBuildMaterial("G4_Al");
-    G4Tubs *solidAl = new G4Tubs("solidAl", detIR, detOR, detHalfHeight, 0, TMath::TwoPi());
+    detOR = detIR + wallThickness;
+    solidAl = new G4Tubs("solidAl", detIR, detOR, detHalfHeight, 0, TMath::TwoPi());
     logicAl = new G4LogicalVolume(solidAl, al, "logicAl");
     physAl = new G4PVPlacement(0, G4ThreeVector(), logicAl, "physAl", logicWorld, false, 0, checkOverlaps);
 
     // PTP
-    G4double ptpHalfHeight = 50*um;
-    G4ThreeVector ptp_z_offset = G4ThreeVector(0, 0, detHalfHeight - ptpHalfHeight);
-    G4Material *ptp = nist->FindOrBuildMaterial("G4_TERPHENYL");
-    G4Tubs *solidPTP = new G4Tubs("solidPTP", 0, detIR, ptpHalfHeight, 0, TMath::TwoPi());
+    G4ThreeVector* ptp_z_offset = new G4ThreeVector(0, 0, detHalfHeight - ptpHalfHeight);
+    solidPTP = new G4Tubs("solidPTP", 0, detIR, ptpHalfHeight, 0, TMath::TwoPi());
     logicPTP = new G4LogicalVolume(solidPTP, ptp, "logicPTP");
-    physPTP = new G4PVPlacement(0, ptp_z_offset, logicPTP, "physPTP", logicWorld, false, 0, checkOverlaps);
+    physPTP = new G4PVPlacement(0, *ptp_z_offset, logicPTP, "physPTP", logicWorld, false, 0, checkOverlaps);
 
     // DCF
-    G4double dcfHalfHeight = 0.1*cm;
-    G4ThreeVector dcf_z_offset = G4ThreeVector(0, 0, detHalfHeight - ptpHalfHeight * 2 - dcfHalfHeight);
-    G4Material *dcf = nist->FindOrBuildMaterial("G4_SILICON_DIOXIDE");
-    G4Tubs *solidDCF = new G4Tubs("solidDCF", 0, detIR, dcfHalfHeight, 0, TMath::TwoPi());
+    G4ThreeVector* dcf_z_offset = new G4ThreeVector(0, 0, detHalfHeight - ptpHalfHeight * 2 - dcfHalfHeight);
+    solidDCF = new G4Tubs("solidDCF", 0, detIR, dcfHalfHeight, 0, TMath::TwoPi());
     logicDCF = new G4LogicalVolume(solidDCF, dcf, "logicDCF");
-    physDCF = new G4PVPlacement(0, dcf_z_offset, logicDCF, "physDCF", logicWorld, false, 0, checkOverlaps);
+    physDCF = new G4PVPlacement(0, *dcf_z_offset, logicDCF, "physDCF", logicWorld, false, 0, checkOverlaps);
 
     // PTP
-    G4double tpbHalfHeight = 50*um;
-    G4ThreeVector tpb_z_offset = G4ThreeVector(0, 0, detHalfHeight - ptpHalfHeight * 2 - dcfHalfHeight * 2 - tpbHalfHeight);
-    G4Material *tpb = nist->FindOrBuildMaterial("G4_ANTHRACENE");
-    G4Tubs *solidTPB = new G4Tubs("solidTPB", 0, detIR, tpbHalfHeight, 0, TMath::TwoPi());
+    G4ThreeVector* tpb_z_offset = new G4ThreeVector(0, 0, detHalfHeight - ptpHalfHeight * 2 - dcfHalfHeight * 2 - tpbHalfHeight);
+    solidTPB = new G4Tubs("solidTPB", 0, detIR, tpbHalfHeight, 0, TMath::TwoPi());
     logicTPB = new G4LogicalVolume(solidTPB, tpb, "logicTPB");
-    physTPB = new G4PVPlacement(0, tpb_z_offset, logicTPB, "physTPB", logicWorld, false, 0, checkOverlaps);
+    physTPB = new G4PVPlacement(0, *tpb_z_offset, logicTPB, "physTPB", logicWorld, false, 0, checkOverlaps);
 
     // PMT
-    G4double pmtHalfHeight = 0.1*cm;
-    G4ThreeVector pmt_z_offset = G4ThreeVector(0, 0, - detHalfHeight + pmtHalfHeight);
-    G4Material *kali = nist->FindOrBuildMaterial("G4_K");
-    G4Tubs *solidPMT = new G4Tubs("solidPMT", 0, detIR, pmtHalfHeight, 0, TMath::TwoPi());
+    G4ThreeVector* pmt_z_offset = new G4ThreeVector(0, 0, - detHalfHeight + pmtHalfHeight);
+    solidPMT = new G4Tubs("solidPMT", 0, detIR, pmtHalfHeight, 0, TMath::TwoPi());
     logicPMT = new G4LogicalVolume(solidPMT, kali, "logicPMT");
-    physPMT = new G4PVPlacement(0, pmt_z_offset, logicPMT, "physPMT", logicWorld, false, 0, checkOverlaps);
+    physPMT = new G4PVPlacement(0, *pmt_z_offset, logicPMT, "physPMT", logicWorld, false, 0, checkOverlaps);
 
     return physWorld;
 }
